26aecbb55ddce0a961e3a61386f80cde68ec6226
eduardocastro31 <112891575+eduardocastro31@users.noreply.github.com>
2024-06-16 22:55:59 -0300

    Arreglo de funcion run()

--- index.js
+++ index.js
@@ -3,7 +3,13 @@ const express= require("express");
 const app = express();
 const { MONGODB_USR, MONGODB_PWD } =  require("./config.js");
 const uri = "mongodb+srv://"+MONGODB_USR+":"+MONGODB_PWD+"@bd.m0u45gc.mongodb.net/?retryWrites=true&w=majority&appName=bd"
-const client = new MongoClient(uri);
+const client = new MongoClient(uri, {
+  serverApi: {
+   version: ServerApiVersion.v1,
+   strict: true,
+   deprecationErrors: true,
+  },
+});
 const bd = client.db("basedatos");
 const personas = bd.collection("personas");
 const path = require("path");
@@ -25,13 +31,13 @@ async function run() {
      // Conecta el cliente al servidor
      await client.connect();
      // Envia un ping para confirmar una conexión exitosa
-   //  await client.db("admin").command({ ping: 1 });
+     await client.db("admin").command({ ping: 1 });
      console.log("Conexion a MongoDB exitosa !");
    }
    catch(error){
       // Asegura que el cliente se cerrará cuando de error
       await client.close()
-     console.log("No se pudo conectar al servidor por el error: ",error)
+     console.log("No se pudo conectar al servidor, error: ",error)
     }}
    run();
 
